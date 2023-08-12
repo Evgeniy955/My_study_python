6c927dbce9e11f6928792879a9525365ec05be59
Evgeniy955 <galitsyn.evgeniy955@gmail.com>
2023-08-12 03:32:48 +0300

    Add send mail file

--- study_2023/send_email_to_slack/send_email_for_slack_oop.py
+++ study_2023/send_email_to_slack/send_email_for_slack_oop.py
@@ -6,15 +6,6 @@ from email.mime.base import MIMEBase
 from email import encoders
 from email.mime.text import MIMEText
 
-
-def create_zip_archive(source_folder, output_filename, output_path):
-    try:
-        os.listdir(source_folder)
-        shutil.make_archive(os.path.join(output_path, output_filename), 'zip', source_folder)
-    except FileNotFoundError:
-        print("\033[91mThe folder path isn't correct\033[0m")
-
-
 source_folder = 'C:\\Users\\halitsyn.y\\Desktop\\backup_original\\Archive'
 output_filename = 'Postman_English'
 output_path = 'C:\\Users\\halitsyn.y\\Documents\\Archives'
@@ -28,38 +19,44 @@ body = "Table with links"
 filename = f"{output_filename}.zip"
 
 
-# Attaching a file
-def attaching_a_file():
-    attachment = open(f"{output_path}\\{filename}", "rb")
-    part = MIMEBase('application', 'octet-stream')
-    part.set_payload(attachment.read())
-    encoders.encode_base64(part)
-    part.add_header('Content-Disposition', f"attachment; filename= {filename}")
-    return part
+class SendMail:
+    def __init__(self, source_folder, output_filename, output_path):
 
+        try:
+            os.listdir(source_folder)
+            shutil.make_archive(os.path.join(output_path, output_filename), 'zip', source_folder)
+        except FileNotFoundError:
+            print("\033[91mThe folder path isn't correct\033[0m")
 
-# Sending email via SMTP
-def send_mail():
-    msg = MIMEMultipart()
-    msg['From'] = from_email
-    msg['To'] = to_email
-    msg['Subject'] = subject
-    msg.attach(MIMEText(body, 'plain'))
-    msg.attach(attaching_a_file())
-    try:
-        with smtplib.SMTP('smtp.office365.com', 587) as server:
-            server.starttls()
-            server.login(from_email, password)
-            server.sendmail(from_email, to_email, msg.as_string())
+    # Attaching a file
+    def attaching_a_file(self):
+        attachment = open(f"{output_path}\\{filename}", "rb")
+        part = MIMEBase('application', 'octet-stream')
+        part.set_payload(attachment.read())
+        encoders.encode_base64(part)
+        part.add_header('Content-Disposition', f"attachment; filename= {filename}")
+        return part
 
-        print("Email sent successfully")
-    except smtplib.SMTPAuthenticationError:
-        print("\033[91mAuthentication unsuccessful, the user credentials were incorrect\033[0m")
-    finally:
-        archive_file = os.path.join(output_path, filename)
-        os.remove(archive_file)
+    # Sending email via SMTP
+    def send_mail(self):
+        msg = MIMEMultipart()
+        msg['From'] = from_email
+        msg['To'] = to_email
+        msg['Subject'] = subject
+        msg.attach(MIMEText(body, 'plain'))
+        msg.attach(SendMail.attaching_a_file(self))
+        try:
+            with smtplib.SMTP('smtp.office365.com', 587) as server:
+                server.starttls()
+                server.login(from_email, password)
+                server.sendmail(from_email, to_email, msg.as_string())
 
+            print("Email sent successfully")
+        except smtplib.SMTPAuthenticationError:
+            print("\033[91mAuthentication unsuccessful, the user credentials were incorrect\033[0m")
+        finally:
+            archive_file = os.path.join(output_path, filename)
+            os.remove(archive_file)
 
-if __name__ == "__main__":
-    create_zip_archive(source_folder, output_filename, output_path)
-    send_mail()
+x = SendMail(source_folder, output_filename, output_path)
+x.send_mail()
