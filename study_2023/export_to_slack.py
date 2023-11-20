d9d02b2d73408abb8d7933b2103028a3435954de
Evgeniy955 <galitsyn.evgeniy955@gmail.com>
2023-11-20 11:59:05 +0200

    new files

--- study_2023/export_to_slack.py
+++ study_2023/export_to_slack.py
@@ -1,13 +1,13 @@
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 
-my_token = "xoxe.xoxp-1-Mi0yLTQ5OTMyMjMwNzEtNTY3MTU0NDE2Mzk2OC01NzI2NzA5MTc0OTQ1LTU3MTQwODAyNjM1NTQtMzg2MWMxN2I4YzQ3OGI4ZGYxZDIyOTBiZDc0OTcyYzBhYmQwYzZiMTgzYzAxYzMxMzNiZjU3MGJmZDBmYmEyMg"
+my_token = "xoxe.xoxp-1-Mi0yLTQ5OTMyMjMwNzEtNTY3MTU0NDE2Mzk2OC01NzI2NzA5MTc0OTQ1LTU4NDAzNjE2OTM2NzEtNGJiNDFmYzVhZmI3YzI3NDkxYWRmYjdhYzczNzdkNmQyOGY0OWM3ZWJiZDhhMWZkMzQ4Y2FiMTlkMTdhZjM2MA"
 client = WebClient(token=my_token)
 
 try:
     response = client.files_upload_v2(
         channel='C05LXP4R55J',
-        file='C:\\Users\\halitsyn.y\\Desktop\\Postman_English.zip',
+        file='C:\\Users\\halitsyn.y\\Desktop\\Postman_English.7z',
         title='Archive File',
         initial_comment='Here is the archive file!'
     )
