7d57f7b6ae78321ff941e5aacac315a02f9c610e
Evgeniy955 <galitsyn.evgeniy955@gmail.com>
2024-01-28 03:27:02 +0200

    add get test run time

--- 2024/add_elem_to_dict/get_sum_time.py
+++ 2024/add_elem_to_dict/get_sum_time.py
@@ -5,11 +5,22 @@ def time_all_runs():
     get_all_time = 0
     with open('db.json') as f:
         data = json.load(f)
-        print(data)
         for elem in data:
-            for name, time in elem.items():
+            for time in elem.values():
                 get_all_time += int(time)
     return get_all_time
 
 
-print(time_all_runs())
+def seconds_to_hms(seconds):
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    return hours, minutes, seconds
+
+
+def result_time():
+    hours, minutes, seconds = seconds_to_hms(time_all_runs())
+    print(f"Run times: {int(hours)} hours, {int(minutes)} minutes, {int(seconds)} seconds")
+
+
+if __name__ == '__main__':
+    result_time()
--- 2024/add_elem_to_dict/get_time.py
+++ 2024/add_elem_to_dict/get_time.py
@@ -4,25 +4,6 @@ import os
 
 file_path = "C:\\Users\\halitsyn.y\\PycharmProjects\\My_study_python\\2024\\add_elem_to_dict\\db.json"
 
-# def get_run_time(time):
-#     name_run = time.split(":")[0]
-#     run_time = time.split(" ")[1]
-#     to_json = {name_run: run_time}
-#
-#     with open('sw_templates.json', 'w') as f:
-#         json.dump(to_json, f)
-#
-#     with open('sw_templates.json') as f:
-#         print(f.read())
-#
-# get_run_time(new_data())
-
-def create_json():
-    json_data = []
-    if not os.path.exists(file_path):
-        with open('db.json', 'w') as file:
-            file.write(json.dumps(json_data))
-
 
 def add_to_json(time):
     name_run = time.split(":")[0]
@@ -31,7 +12,16 @@ def add_to_json(time):
     data = json.load(open("db.json"))
     data.append(json_data)
     with open("db.json", "w") as file:
-        json.dump(data, file, indent=2, ensure_ascii=False)
+        json.dump(data, file, indent=2)
 
-create_json()
-add_to_json(new_data())
+
+def create_json(new_time):
+    json_data = []
+    if not os.path.exists(file_path):
+        with open('db.json', 'w') as file:
+            file.write(json.dumps(json_data))
+    else:
+        add_to_json(new_time)
+
+
+create_json(new_data())
