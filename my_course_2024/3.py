72f3400a36040b2d17f3dac9c903bdb3d755d051
Yevhen <galitsyn.evgeniy955@gmail.com>
2024-09-15 23:09:58 +0300

    my_course

--- my_course_2024/3.py
+++ my_course_2024/3.py
@@ -5,10 +5,9 @@ def task_3():
     changed_string = ""
     count = 0
     for s in text:
-        if s.lower().isalnum():
+        if s.lower().isalpha():
             changed_string += s
-        for letter in vowels:
-            if s == letter:
+            if s in vowels:
                 count += 1
 
     print("Number of letters in a line: ", len(changed_string))
@@ -17,3 +16,8 @@ def task_3():
 
 if __name__ == '__main__':
     task_3()
+
+    # letters_count = len([char for char in text if char.isalpha()])
+    # vowels = "aeiouAEIOU"
+    # vowels_count = len([char for char in text if char in vowels])
+    # modified_text = text.replace(" ", "-")
