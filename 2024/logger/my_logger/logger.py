74cecb2de2a438c50e580744dd4c1fc1883420aa
Yevhen <galitsyn.evgeniy955@gmail.com>
2024-11-10 14:51:42 +0200

    new loggers

---
+++ 2024/logger/my_logger/logger.py
@@ -0,0 +1,79 @@
+import logging
+from colorama import Fore, Style, init
+
+init(autoreset=True)
+
+class ColoredFormatter(logging.Formatter):
+    def __init__(self, fmt, datefmt=None):
+        super().__init__(fmt, datefmt)
+        self.FORMATS = {
+            logging.DEBUG: Fore.BLUE + fmt + Style.RESET_ALL,
+            logging.INFO: Fore.GREEN + fmt + Style.RESET_ALL,
+            logging.WARNING: Fore.YELLOW + fmt + Style.RESET_ALL,
+            logging.ERROR: Fore.RED + fmt + Style.RESET_ALL,
+            logging.CRITICAL: Fore.MAGENTA + fmt + Style.RESET_ALL,
+        }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno, self._fmt)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+class AppLogger:
+    _logger = None
+
+    @staticmethod
+    def _initialize(name="app_logger", log_file="app_log_file.log"):
+        logger = logging.getLogger(name)
+        logger.setLevel(logging.DEBUG)
+
+        if not logger.handlers:
+            file_handler = logging.FileHandler(log_file)
+            file_handler.setLevel(logging.DEBUG)
+            formatter_str = "%(asctime)s - %(levelname)s - %(message)s"
+            file_handler.setFormatter(logging.Formatter(formatter_str))
+            logger.addHandler(file_handler)
+
+            console_handler = logging.StreamHandler()
+            console_handler.setLevel(logging.DEBUG)
+            console_handler.setFormatter(ColoredFormatter(formatter_str))
+            logger.addHandler(console_handler)
+
+        return logger
+
+    @classmethod
+    def get_logger(cls):
+        if cls._logger is None:
+            cls._logger = cls._initialize()
+        return cls._logger
+
+    @staticmethod
+    def debug(message):
+        AppLogger.get_logger().debug(message)
+
+    @staticmethod
+    def info(message):
+        AppLogger.get_logger().info(message)
+
+    @staticmethod
+    def warning(message):
+        AppLogger.get_logger().warning(message)
+
+    @staticmethod
+    def error(message):
+        AppLogger.get_logger().error(message)
+
+    @staticmethod
+    def critical(message):
+        AppLogger.get_logger().critical(message)
+
+logger = AppLogger
+
+# Пример использования
+# if __name__ == '__main__':
+#     logger = AppLogger.get_logger()
+#     logger.debug("This is a debug message")
+#     logger.info("This is an info message")
+#     logger.critical("This is a critical message")
+#     logger.warning("This is a warning message")
+#     logger.error("This is an error message")
